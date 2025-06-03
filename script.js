e7ada694bbd6bfb2f3a7f969f1697828f638c464
shariq-ali-30 <shariq3072007@gmail.com>
2025-06-03 17:24:43 +0500

    Update script.js

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ async function getWeather(cityName) {
 
     document.querySelector('.temp h1').innerText = Math.round(data.current.temp_c) + '°C';
 document.querySelector('.temp h2').innerText = data.location.name + ', ' + data.location.country;
+document.querySelector('.temp p').innerText = 
 document.querySelector('.humidity').innerText = data.current.humidity + '%';
 document.querySelector('.wind').innerText = data.current.wind_kph + ' km/h';
 
